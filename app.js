f238bf349edcb05f0665d280beeb83f5e1d8e090
programmingfire@gmail.com <squadguy@github.com>
2015-09-13 19:26:05 -0500

    Add comments to app.js

--- app.js
+++ app.js
@@ -6,17 +6,18 @@ var app = express();
 
 //configure app
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs'); //needed for embedded js used in index.ejs
+app.set('views', path.join(__dirname, 'views'));//ties view with directory views
 
 //use middleware
 
-app.use(express.static(path.join(__dirname, 'bower_components')));
+app.use(express.static(path.join(__dirname, 'bower_components')));//ties static files in views with bower_components directory
 
 app.use(bodyParser());
 
 //define routes
 
+//  dummy data
 var todoItems = [
 	{ id: 1, desc: 'foo' },
 	{ id: 2, desc: 'bar' },
@@ -24,19 +25,19 @@ var todoItems = [
 	];
 
 app.get('/', function(req, res){
-	// load data from DB here	
+	// load data from "DB" here	
 	res.render('index', {
 		title: 'My App',
 		items: todoItems
 	});
-	console.log('Got a hit!');
+	console.log('Got a hit!'); //logs all incoming requests to server
 });
 
 app.post('/add', function (req, res) {
-	var newItem = req.body.newItem;
+	var newItem = req.body.newItem;//middleware bodyParser allows for body method
 	
 	todoItems.push({
-		id: todoItems.lenghth + 1,
+		id: todoItems.length + 1,
 		desc: newItem
 	});
 	
